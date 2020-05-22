955f9d27872350316c9e3bcb382658146b4e1f8c
mscates <32601042+Friendofmario@users.noreply.github.com>
2020-05-22 08:15:40 -0700

    Update bannerScanner.py

--- bannerScanner.py
+++ bannerScanner.py
@@ -2,42 +2,47 @@
 import socket
 import os
 import sys
-filename = "banners.txt"
-def retBanner(ip,port):
-    try:
-        socket.setdefaulttimeout(2)
-        s = socket.socket()
-        s.connect((ip.port))
-        banner = s.recv(1024)
-        return banner
-    except:
-        return
 
-def checkVulns(banner,filename):
+def checkVulns(filename,banner):
     f = open(filename, "r")
     for line in f.readLines():
         if line.strip("\n") in banner:
             print "server is vulnerable: " + banner.strip("\n")
+
+def retBanner(ip,port):
+    try:
+	print "h"
+        socket.setdefaulttimeout(2)
+        s = socket.socket()
+        s.connect((ip,port))
+        banner = s.recv(1024)
+	print banner
+        return banner
+    except:
+	print "oh no"
+        return
 
 def main():
 	if len(sys.argv) == 2:
 		filename = sys.argv[1]
 		if not os.path.isfile(filename):
 			print 'File doesnt exist'
-                exit(0)
 		if not os.access(filename, os.R_OK):
 			print 'Access Denied'
 			exit(0)
 	else:
 		print 'Usage: ' + str(sys.argv[0]) + '<vuln filename>'
 		exit(0)
-	portlist = 1000
-	for x in range(1,255):
-    		ip = socket.gethostbyname(socket.gethostname()) + str(x)
+	portlist = 10
+	for x in range(0,10):
+    		ip0 = socket.gethostbyname(socket.gethostname()).split(".")[0] + "."
+		ip1 = ip0 + socket.gethostbyname(socket.gethostname()).split(".")[1] + "."
+		ip2 = ip1 + socket.gethostbyname(socket.gethostname()).split(".")[2] + "."
+		ip = ip2 + str(x)
+		print ip
 		for port in range(1, portlist):
 			banner = retBanner(ip,port)
 			if banner:
 				print ip + '/' + str(port) + ':' + banner
 				checkVulns(filename, banner)
-main()
-			
+main()		
